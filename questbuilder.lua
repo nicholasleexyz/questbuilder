009f3ad5b1b99d21dfaf9997704ad1d92eaf21ab
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-08 01:24:04 -0600

    improved menu system

--- questbuilder.lua
+++ questbuilder.lua
@@ -1,8 +1,24 @@
 -- Quest Generator
 --
 
+function printhelp()
+	local line = "--------\n"
+	local a = "a or add - add in a new quest.\n"
+	local c = "c or complete - complete quest by id.\n"
+	local r = "r or remove - remove quest by id.\n"
+	local q = "q or quit - exit program.\n"
+	local h = "h or help - print help menu.\n"
+	io.write(line..h..a..c..r..q..line);
+end
+
 function printquest(quest, id)
-	local text = "--------\nQUEST ID: "..id.."\n"..quest[1].." \nREWARD: "..quest[2].."\n STATUS: "..(quest[3] and "COMPLETED" or "INCOMPLETE\n--------\n")
+	local line = "--------\n"
+	local idtext = "QUEST ID: "..id.."\n"
+	local questtext = quest[1].."\n"
+	local rewardtext = "REWARD: "..quest[2].."\n"
+	local statustext = "STATUS: "..(quest[3]).."\n"
+	local text = line..idtext..questtext..rewardtext..statustext..line
+	--"--------\nQUEST ID: " ..id.."\n" ..quest[1].."\n" .."REWARD: "..quest[2].."\n" .."STATUS: "..(quest[3] and "COMPLETED" or "INCOMPLETE\n--------\n"
 	io.write(text)
 end
 
@@ -20,7 +36,7 @@ function buildrandomquest()
 		task = "Collect "..math.random(10).." "..getrandom(items).."(s)."
 	end
 
-	return {task, reward, false}
+	return {task, reward, "INCOMPLETE"}
 end
 
 function getrandom(tab)
@@ -34,19 +50,38 @@ items={"gold coin", "apple", "shovel", "sword", "ring", "hat!"}
 running = true
 questlog = {} -- contains quests :P
 
+printhelp()
+
 while(running)
 do
 	local input = io.read()
-	if(input == 'q') then
+	if(input == "quit" or input == "q") then
 		running = false
 		return
-	end
-	if(input == 'a') then
+	elseif(input == "add" or input == "a") then
 		local q = buildrandomquest()
 		questlog[#questlog + 1] = q
 		for i, v in pairs(questlog) do
 			printquest(v, i)
 		end
+	elseif(input == "remove" or input == "r") then
+		io.write("Remove which quest? ID: ")
+		input = io.read()
+		local num = tonumber(input)
+		table.remove(questlog, num)
+		for i, v in pairs(questlog) do
+			printquest(v, i)
+		end
+	elseif(input == "complete" or input == "c") then
+		io.write("Complete which quest? ID: ")
+		input = io.read()
+		local num = tonumber(input)
+		questlog[num][3] = "COMPLETE"
+		for i, v in pairs(questlog) do
+			printquest(v, i)
+		end
+	elseif(input == "help" or input == "h") then
+		printhelp()
 	end
 end
 
